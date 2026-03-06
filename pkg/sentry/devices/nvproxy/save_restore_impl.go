e08ce0a9cd353f3f34e3ed1b0fc5ed4dc60912c3
Matt Nappo <matt@modal.com>
2026-03-06 23:15:20 +0000

    Try close frontend FDs

--- pkg/sentry/devices/nvproxy/save_restore_impl.go
+++ pkg/sentry/devices/nvproxy/save_restore_impl.go
@@ -20,9 +20,14 @@ package nvproxy
 import (
 	goContext "context"
 	"fmt"
+	"path/filepath"
 	"strings"
 
+	"golang.org/x/sys/unix"
 	"gvisor.dev/gvisor/pkg/context"
+	"gvisor.dev/gvisor/pkg/devutil"
+	"gvisor.dev/gvisor/pkg/fdnotifier"
+	"gvisor.dev/gvisor/pkg/log"
 	"gvisor.dev/gvisor/pkg/sentry/kernel"
 	"gvisor.dev/gvisor/pkg/sentry/vfs"
 )
@@ -93,14 +98,38 @@ func nvidiaFDHolders(k *kernel.Kernel, match func(vfs.FileDescriptionImpl) bool)
 }
 
 func (fd *frontendFD) beforeSaveImpl() {
-	holders := nvidiaFDHolders(fd.dev.nvp.k, func(impl vfs.FileDescriptionImpl) bool {
-		return impl == fd
-	})
-	panic(fmt.Sprintf("nvproxy.frontendFD is not saveable (hostFD=%d, dev=%s); holders: %s", fd.hostFD, fd.dev.basename(), holders))
+	fd.dev.nvp.clientsMu.RLock()
+	hasClients := len(fd.clients) > 0
+	fd.dev.nvp.clientsMu.RUnlock()
+
+	fd.memmapFile.mmapMu.Lock()
+	hasMmap := fd.memmapFile.mmapLength > 0
+	fd.memmapFile.mmapMu.Unlock()
+
+	if hasClients || hasMmap {
+		holders := nvidiaFDHolders(fd.dev.nvp.k, func(impl vfs.FileDescriptionImpl) bool {
+			return impl == fd
+		})
+		panic(fmt.Sprintf("nvproxy.frontendFD is not saveable (hostFD=%d, dev=%s, hasClients=%t, hasMmap=%t); holders: %s",
+			fd.hostFD, fd.dev.basename(), hasClients, hasMmap, holders))
+	}
+	log.Infof("nvproxy: saving %s (hostFD=%d) with no clients and no mmap; will reopen on restore", fd.dev.basename(), fd.hostFD)
 }
 
-func (fd *frontendFD) afterLoadImpl(goContext.Context) {
-	panic("nvproxy.frontendFD is not restorable")
+func (fd *frontendFD) afterLoadImpl(ctx goContext.Context) {
+	devName := fd.dev.basename()
+	hostFD, containerName, err := reopenHostDev(ctx, devName, fd.dev.nvp.useDevGofer)
+	if err != nil {
+		panic(fmt.Sprintf("nvproxy: reopening %s on restore: %v", devName, err))
+	}
+	fd.hostFD = hostFD
+	fd.containerName = containerName
+	fd.memmapFile.SetFD(int(fd.hostFD))
+	if err := fdnotifier.AddFD(fd.hostFD, &fd.internalQueue); err != nil {
+		unix.Close(int(fd.hostFD))
+		panic(fmt.Sprintf("nvproxy: fdnotifier for restored %s (hostFD=%d): %v", devName, fd.hostFD, err))
+	}
+	log.Infof("nvproxy: restored %s with new hostFD=%d", devName, fd.hostFD)
 }
 
 func (fd *uvmFD) beforeSaveImpl() {
@@ -112,4 +141,25 @@ func (fd *uvmFD) beforeSaveImpl() {
 
 func (fd *uvmFD) afterLoadImpl(goContext.Context) {
 	panic("nvproxy.uvmFD is not restorable")
+}
+
+// reopenHostDev reopens a host /dev device during restore.
+func reopenHostDev(ctx goContext.Context, relpath string, useDevGofer bool) (int32, string, error) {
+	if useDevGofer {
+		devClient := devutil.GoferClientFromContext(ctx)
+		if devClient == nil {
+			return -1, "", fmt.Errorf("dev gofer client unavailable")
+		}
+		hostFD, err := devClient.OpenAt(context.Background(), relpath, unix.O_RDWR)
+		if err != nil {
+			return -1, "", fmt.Errorf("gofer OpenAt: %w", err)
+		}
+		return int32(hostFD), devClient.ContainerName(), nil
+	}
+	abspath := filepath.Join("/dev", relpath)
+	hostFD, err := unix.Openat(-1, abspath, unix.O_RDWR|unix.O_NOFOLLOW, 0)
+	if err != nil {
+		return -1, "", fmt.Errorf("openat %s: %w", abspath, err)
+	}
+	return int32(hostFD), "", nil
 }
